8702c1bf6315e50c44884d51621c9043095f9c10
chenfei <chenfeigogo@gmail.com>
2018-08-20 01:24:41 +0800

    0.0.4

--- PhotoBrowser/ZLDefine.h
+++ PhotoBrowser/ZLDefine.h
@@ -72,7 +72,7 @@
 
 // 图片路径
 #define kZLPhotoBrowserSrcName(file) [@"ZLPhotoBrowser.bundle" stringByAppendingPathComponent:file]
-#define kZLPhotoBrowserFrameworkSrcName(file) [@"Frameworks/ZLPhotoBrowser.framework/ZLPhotoBrowser.bundle" stringByAppendingPathComponent:file]
+#define kZLPhotoBrowserFrameworkSrcName(file) [@"Frameworks/CFZLPhotoBrowser.framework/ZLPhotoBrowser.bundle" stringByAppendingPathComponent:file]
 
 #define kViewWidth      [[UIScreen mainScreen] bounds].size.width
 #define kViewHeight     [[UIScreen mainScreen] bounds].size.height
